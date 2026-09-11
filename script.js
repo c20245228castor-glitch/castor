35f8f3d117f726acc04f2a6b0b89e9fc75356017
potatoo <c20245228.castor@csav.edu.ph>
2026-09-11 09:20:11 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world.");
+console.log("Hello mundo")
+const Myname = "Diolito";
+console.log("name: " + Myname);
+let age = 20;
+let number = "09109436835";
+console.log("Age: " + age)
+console.log("Number: " + number);
+
+const address = "Brgy. San Isidro E.B. Magalona";
+console.log("Address: " + address);
